c09db066441ea0ad4037a1db61685ed58ce66e3e
Gustavo Ferreira <gustavo.ferreira@redetendencia.com.br>
2021-03-31 18:08:51 -0400

    add cross origin nos endpoints

--- src/main/java/br/com/gustavo/controller/UtensilioController.java
+++ src/main/java/br/com/gustavo/controller/UtensilioController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @Controller
+@CrossOrigin(origins = "http://localhost:8081")
 @RequestMapping("/utensilio")
 public class UtensilioController {
 
